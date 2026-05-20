a29cc0fadc0b1f70f77a339b9b5ccc62b643f910
ANGEL AZAMAR <arq.azamar@gmail.com>
2026-05-20 07:40:13 -0600

    Update professional iPhone icon 2.0.2

--- public/sw.js
+++ public/sw.js
@@ -5,6 +5,10 @@ const APP_SHELL = [
   '/index.html',
   '/manifest.webmanifest',
   '/icons/icon.svg',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png',
+  '/apple-touch-icon.png',
+  '/favicon.svg',
   '/screenshots/mobile.svg'
 ];
 
